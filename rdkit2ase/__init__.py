57fb76a1039e70bba495184ca132494a03309f03
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-05-23 18:55:52 +0200

    refactor package

--- rdkit2ase/__init__.py
+++ rdkit2ase/__init__.py
@@ -1,4 +1,5 @@
 from .rdkit2ase import ase2rdkit, rdkit2ase
-from .utils import pack, smiles2atoms, smiles2conformers
+from .smiles import smiles2atoms, smiles2conformers
+from .pack import pack
 
 __all__ = ["rdkit2ase", "ase2rdkit", "smiles2atoms", "pack", "smiles2conformers"]

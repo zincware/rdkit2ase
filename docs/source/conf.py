9e45c6745c7d807d796c43ce6cca559c1a2faabd
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2025-06-23 18:10:04 +0200

    Docs (#45)
    * add sphinx
    
    * build pages docs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * install pandoc
    
    * fix path
    
    * deploy again?
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * push on main
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import typing as t
+from importlib.metadata import metadata
+
+pkg_metadata = metadata("rdkit2ase")
+
+project = "rdkit2ase"
+author = "Fabian Zills"
+version = pkg_metadata["Version"]
+release = f"v{version}"
+copyright = f"2025, {author}"
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["nbsphinx"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_title = project
+html_short_title = project
+# html_favicon = "_static/mlipx-favicon.svg"
+
+html_theme_options: t.Dict[str, t.Any] = {
+    # "light_logo": "mlipx-light.svg",
+    # "dark_logo": "mlipx-dark.svg",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/zincware/rdkit2ase",
+            "html": "",
+            "class": "fa-brands fa-github fa-2x",
+        },
+    ],
+    "source_repository": "https://github.com/zincware/rdkit2ase",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+    "navigation_with_keys": True,
+}
+
+# font-awesome logos
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,14 @@
+rdkit2ase Documentation
+=======================
+
+The rdkit2ase package provides an interface between RDKit and ASE.
+It further integrates packmol and networkx features and molecular mapping.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   ase_tools
+   rdkit_tools
+   packmol_tools
+   networkx_tools
